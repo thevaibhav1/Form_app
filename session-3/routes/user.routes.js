e2e7091b1670ca872344d8f7535da92adcc9e538
vaibhav sahu <52112861+thevaibhav1@users.noreply.github.com>
2025-03-26 23:11:54 +0530

    Delete session-3 directory

--- session-3/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { getUser, getUserById } = require("../controller/user.controler");
-
-router.get("/", getUser);
-router.get("/:uuid", getUserById);
-
-module.exports = router;
